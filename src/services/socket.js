6b54aa5c4ce935a5eb4a2cd02075f7baab24e313
NextGrid Technologies <242390951+Chikanyac@users.noreply.github.com>
2026-06-10 11:49:57 +0200

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,21 +1,15 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://api.bidup.co.zw"; // change to production later
+const SOCKET_URL = "https://api.bidup.co.zw";
 
 let socket;
 
-export const initSocket = () => {
-  if (!socket) {
-    socket = io(SOCKET_URL, {
-      transports: ["websocket"]
-    });
-  }
-  return socket;
-};
-
 export const getSocket = () => {
   if (!socket) {
-    return initSocket();
+    socket = io(SOCKET_URL, {
+      transports: ["websocket"],
+      withCredentials: true
+    });
   }
   return socket;
 };
